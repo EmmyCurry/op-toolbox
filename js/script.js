553cc8e37888b162d4fac5e1e3103d2556217bf5
Emmy Curry <emmycurry86@gmail.com>
2023-10-17 09:47:34 -0400

    got a framework working!

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const device = document.getElementById('deviceType').value;
 
 
-    const othersPresent = document.getElementById('othersPresent');
-    othersPresent.addEventListener('click', () => {
-        const sentenceOne = document.getElementById('sentence-one')
-        sentenceOne.innerText = 'whole thing text changed';
-        
-        //'&lt;&lt;first/last name&gt;&gt; was seen at &lt;&lt;visit location&gt;&gt; with additional persons present on &lt;&lt;visit date&gt;&gt; for &lt;&lt;visit type&gt;&gt; of [device].'
+const checkbox = document.getElementById('othersPresent');
+checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+        const sentenceOne = document.getElementById('sentence-one');
+        sentenceOne.innerText = '<<first/last name>> was seen at <<visit location>> on <<visit date>> for <<visit type>> of ' + device + '. They were accompanied by '
+        ;
+    } else {
+        const sentenceOne = document.getElementById('sentence-one');
+        sentenceOne.innerText = '<<first/last name>> was seen at <<visit location>> on <<visit date>> for <<visit type>> of ' + device + '.'
 
-    })
+    }
+});
+
+}); //closing parentheses for DOMContentLoaded
+
+
+
 
 
 //appendchild to insert another question
 
-const peopleAttended = document.getElementById('othersPresent').value //value for new question added ("who was in attendance?")
+// const peopleAttended = document.getElementById('othersPresent').value //value for new question added ("who was in attendance?")
 
-sentenceOne.innerText = '&lt;&lt;first/last name&gt;&gt; was seen at &lt;&lt;visit location&gt;&gt; on &lt;&lt;visit date&gt;&gt; for &lt;&lt;visit type&gt;&gt; of [device]. The appointment was also attended by' + peopleAttended
+// sentenceOne.innerText = '&lt;&lt;first/last name&gt;&gt; was seen at &lt;&lt;visit location&gt;&gt; on &lt;&lt;visit date&gt;&gt; for &lt;&lt;visit type&gt;&gt; of [device]. The appointment was also attended by' +peopleAttended;
